a8bc1e10352758996f9f6dd3f38d40e0e2c73a41
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 16:38:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(number)
   numbers = [1,2,3]
   counter = 1
-  number.each{|number| new_array.push ()#{number}*#{number})}
-  return new_array
+  number.each do |number|
+  puts "#{number} * #{number}"
   counter += 1
- 
+  return square_numbers
 end
 end
