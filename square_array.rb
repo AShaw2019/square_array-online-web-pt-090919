475fc01c885150baec7625f15699fe691c879e8f
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 16:27:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,7 @@
 def square_array(number)
-  numbers = [1]
+  numbers = [1,2,3]
   number.each do [number]
   puts "#{number} * #{number}"
 end
- numbers = [2]
- number.each do [number]
-  puts "#{number} * #{number}"
-end
-  numbers = [3]
-  number.each do [number]
-  puts "#{number} * #{number}"
-end
+ 
 end
